ae04783c1910a019caa3ec2f2543823f6de61b45
Nitya-Somani <nityasahajisomani@gmail.com>
2023-07-15 22:06:35 +0530

    React | Episode-2 [React  Component Introduced ]

--- App.js
+++ App.js
@@ -1,34 +1,53 @@
-/*
-Trying to render 
+import React from "react";
+import ReactDOM from "react-dom";
 
- <h1 id = "parent"> hi i am parent 
- <h2 id = "child1">hello there i m child 1
- <h3 id = "child2">"hi there "
-</h3> 
- <h4 id = "child3">"hi there child-3"
-</h4></h2>
- </h1> 
+//This is react element
+const headingJSX = <h1 className="heading">hello there namaste react !!!!</h1>;
 
+/* 
 
+There are two types of React component 
+1. Class  Based component 
+2. Functional component
+
+/*This is Functional react component
+ React component always starts from capital letters.
+ we can describe it as a function that returns some JSX or react element is a functional component 
+ React functional component at the end of the day is the javascript function .
+ */
+const HeadingComponent = () => {
+  return <h1 className="heading">HI THERE I AM REACT FUNCTIONAL COMPONENT </h1>;
+};
+
+//this syntax is similar to the above syntax  without return
+
+const HeadingComponent2 = () => (
+  <h1>HI THERE I AM WITHOUT RETURN FUNCTIONAL COMPONENT </h1>
+);
+
+//How to render a component  ?
+/* const root = ReactDOM.createRoot(document.getElementById("root"));
+This is how you render a component because babel understands the react component like this
+and then compile it or transpile it to javascript   
+root.render(<HeadingComponent/>); */
+
+//what is component composition ?
+/* A component inside a component is known as component composition
+ or we can say a component inside the other component .
+for eg if we have to include title component inside heading component  
 */
 
+const Title = () => {
+  return <h1> Title here !!!!</h1>;
+};
 
+const Heading = () => (
+  <div id="conatainer">
+    <Title />
+    <h2> I AM heading !!!!</h2>
+    <h3>what is the difference between heading and title &#128512;</h3>
+  </div>
+);
 
-
-const heading = React.createElement(
-    "h1",
-    { id: "parent" },
-    React.createElement(
-        "h2",
-        { id: "child1" },
-        [React.createElement(
-            "h3",
-            { id: "child3" },
-            "hi there"), React.createElement(
-                "h4",
-                { id: "child4" }, "child4" 
-            )]
-        )
-    );
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<Heading/>);
