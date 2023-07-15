68417f65b3869f1fc99e57c79c5cea14d5a50edd
Nitya-Somani <nityasahajisomani@gmail.com>
2023-07-15 23:50:29 +0530

    React | Episode -3 [Root.render () explained ]

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 //This is react element
 const headingJSX = <h1 className="heading">hello there namaste react !!!!</h1>;
@@ -37,17 +37,48 @@ root.render(<HeadingComponent/>); */
 for eg if we have to include title component inside heading component  
 */
 
+
+
 const Title = () => {
   return <h1> Title here !!!!</h1>;
 };
 
+//Three ways to include the functional component in a component -
+/*
+These three ways are the same ways  and will do the same job 
+1. <Title/>
+2.<Title></Title>
+This 3. way is valid because with the help of curly brackets in JSX 
+we used to render  javascript and functional component at the end of the day 
+is a javascript function so yes in JSX we can call that function so it is valid -
+3.{Title()}
+
+*/
 const Heading = () => (
   <div id="conatainer">
     <Title />
+    <Title></Title>
+    {Title()}
     <h2> I AM heading !!!!</h2>
     <h3>what is the difference between heading and title &#128512;</h3>
   </div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Heading/>);
+//ques - can we have to root render and if yes what will happen ?
+
+/* when we will have two root render ideally , In react we should have one root render 
+but if  you will include like this then it will render the content of <Title /> only the one which is at last 
+The render() method is intended to render the initial component hierarchy into the root element, 
+but subsequent calls to render() will replace the previously rendered content.
+
+In your example code, when you call root.render(<Heading />) and then root.render(<Title />), 
+the second call to render() will replace the previously rendered <Heading />
+component with the <Title /> component.
+As a result, only the content of <Title /> will be rendered, and the <Heading />
+component will not be visible.
+
+*/
+
+root.render(<Heading />);
+root.render(<Title />);
