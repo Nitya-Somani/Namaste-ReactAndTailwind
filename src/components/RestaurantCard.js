4f4bf1c5eef4d6fbb168fa28b1d85943bd52b0bf
Nitya-Somani <nityasahajisomani@gmail.com>
2023-08-13 23:41:14 +0530

    React | Adding styles to card

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -2,26 +2,32 @@ import { IMG_URL } from "../utils/commonLinks";
 
 const RestaurantCard = (resData) =>
 { 
-  const {cloudinaryImageId,name,avgRating,cuisines,costForTwo,areaName} = resData?.resData.info;
+  const {cloudinaryImageId,name,avgRating,cuisines,costForTwo,areaName,locality} = resData?.resData.info;
+  const {deliveryTime,serviceability} = resData?.resData.info.sla;
 
   return (
-    <div className="res-card">
-      
-      <div className="resCard-img">
-      <img src={IMG_URL+cloudinaryImageId} alt="res-card-img" />
-      <h3>{name}</h3>
-      <h5>{avgRating} stars</h5>
-      <h5>{cuisines.join(", ")}</h5>
-      <h5>{costForTwo}</h5>
-      <h5>{areaName}</h5>
-      
-      
-
+  <div className ="cards-container">
+    <div className = "card">
+      <div className = "card-media">
+      <img src={IMG_URL+cloudinaryImageId} alt="res-card-img" />  
+        <div className = "delivery-time">{deliveryTime}</div>
+        <div className = "bookmark"></div>
       </div>
-      <div>
-
+      <div className = "card-description">
+        <div className = "about-place">
+          <div className = "place">
+            <p className = "place-name">{name}</p>
+            <p className = "place-speciality">{cuisines.join(", ")}</p>
+          </div>
+          <div className = "place-review">
+            <p className = "rating">{avgRating}  &#x2605;</p>
+            <p className = "per-person">  {costForTwo}</p>
+          </div>
+        </div>
       </div>
     </div>
+  </div>
+    
   )
 }
 
