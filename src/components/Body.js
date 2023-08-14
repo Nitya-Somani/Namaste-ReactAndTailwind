026ef34c15b67b454c4bdf9987ea114e22a72ef5
Nitya-Somani <nityasahajisomani@gmail.com>
2023-08-14 23:41:50 +0530

    Adding a button html and css to filter

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,7 +5,12 @@ import RestaurantCard from "./RestaurantCard";
 const Body = () => {
   return (
     <div className="body">
-      <div className="searchBar">Search Bar</div>
+      <button class="filterButton">
+    <span class="hover-underline-animation"> Prime Dines </span>
+    <svg viewBox="0 0 46 16" height="10" width="30" xmlns="http://www.w3.org/2000/svg" id="arrow-horizontal">
+        <path transform="translate(30)" d="M8,0,6.545,1.455l5.506,5.506H-30V9.039H12.052L6.545,14.545,8,16l8-8Z" data-name="Path 10" id="Path_10"></path>
+    </svg>
+</button>
       <div className="restaurantContainer">
         {/*to include the cards of restaurant we are  including restaurant card component  */}
         {
