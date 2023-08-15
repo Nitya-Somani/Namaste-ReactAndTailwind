0d2d36fe07fa6d0cc7601781281cf688ac42841c
Nitya-Somani <nityasahajisomani@gmail.com>
2023-08-15 13:43:12 +0530

    React | New logo added

--- src/utils/commonLinks.js
+++ src/utils/commonLinks.js
@@ -1,4 +1,4 @@
 
-export const LOGO_URL = "https://designcontest.nyc3.digitaloceanspaces.com/data/contests/301603/entries/c4913704c96cbb55.png"
+export const LOGO_URL = "https://media.istockphoto.com/id/1369371090/vector/letter-ns-restaurant-logo-with-chef-hat-spoon-and-fork-template-restaurant-logo-on-letter-ns.jpg?s=2048x2048&w=is&k=20&c=XQ-lDlJmsKOmptFZ3KTL-VyY0xRoyDyFlXBQ8ViSIaE="
 
 export const IMG_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
